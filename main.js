06c1ba64bc330833b338f7b893d84053a8f4ed3c
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-04-28 22:20:02 +0330

    Implement skeleton animation for right-to-left (RTL) layout

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-document.getElementById("toggleButton").addEventListener("click", function () {
-  var sidebar = document.getElementById("sidebar");
-  sidebar.classList.toggle("open");
-  if (sidebar.classList.contains("open")) {
-    sidebar.style.width = "50%";
-  } else {
-    sidebar.style.width = "0";
-  }
-});
+
